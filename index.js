1e910e363e250245d22e97ae9363d6680e229c41
Chief <brianmwangi10808@gmail.com>
2024-02-10 09:59:30 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create a new <h1> element with id 'victory' containing the message "YOUR-NAME is the champion"
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
